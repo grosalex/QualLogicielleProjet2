7a52d8219c2288ac6e32a1165fb88b1526c1d3a0
Willy91 <williamarques91@gmail.com>
2015-12-08 18:05:20 +0100

    added print and tests in hanoi class

--- src/Hanoi.java
+++ src/Hanoi.java
@@ -9,13 +9,14 @@ public class Hanoi {
 			
 		}
 		for(int i =number;i<0;i--)
-		slots[0].push(i);
+			slots[0].push(i);
 	}
 	
 	public void moveADisk(int from, int to){
-		if(slots[to].head()>slots[from].head()){
+		if(slots[to].head()>slots[from].head() && to>=0 && to<=3 && from>=0 && from<=3 && !slots[from].isEmpty()){
+			System.out.println("Moving disc"+slots[to].head()+" from tower "+to+" into tower "+from + " with disc " + slots[from].hashCode() + "on top.");
 			slots[to].push(slots[from].head());
-			slots[from].pop();
+			slots[from].pop();			
 		}
 	}
 

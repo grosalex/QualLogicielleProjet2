5a43de2aa6422db6e691139f05677fb8b06508be
Willy91 <williamarques91@gmail.com>
2015-12-08 18:25:18 +0100

    added test

--- src/Hanoi.java
+++ src/Hanoi.java
@@ -9,16 +9,23 @@ public class Hanoi {
 			
 		}
 		for(int i =number;i<0;i--)
-			slots[0].push(i);
+			try {
+				slots[0].push(i);
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			}
 	}
 	
 	public void moveADisk(int from, int to){
-		if(slots[to].head()>slots[from].head() && to>=0 && to<=3 && from>=0 && from<=3 && !slots[from].isEmpty()){
-			System.out.println("Moving disc"+slots[to].head()+" from tower "+to+" into tower "+from + " with disc " + slots[from].hashCode() + "on top.");
-			slots[to].push(slots[from].head());
-			slots[from].pop();			
+		try {
+			if(slots[to].head()>slots[from].head() && to>=0 && to<=3 && from>=0 && from<=3 && !slots[from].isEmpty()){
+				System.out.println("Moving disc"+slots[to].head()+" from tower "+to+" into tower "+from + " with disc " + slots[from].hashCode() + "on top.");
+				slots[to].push(slots[from].head());
+				slots[from].pop();			
+			}
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
 		}
 	}
-
 
 }
